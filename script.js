377725509772276b319ae25116e8b06f10354ab1
ValiHakuryuko <98451323+ValiHakuryuko@users.noreply.github.com>
2025-07-22 05:01:32 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
-const toggleButton = document.getElementById("theme-toggle");
+const toggle = document.getElementById("themeToggle");
 const body = document.body;
 
-toggleButton.addEventListener("click", () => {
-  body.classList.toggle("light");
-  body.classList.toggle("dark");
+toggle.addEventListener("change", () => {
+  if (toggle.checked) {
+    body.classList.add("dark-mode");
+    localStorage.setItem("theme", "dark");
+  } else {
+    body.classList.remove("dark-mode");
+    localStorage.setItem("theme", "light");
+  }
+});
+
+window.addEventListener("DOMContentLoaded", () => {
+  const savedTheme = localStorage.getItem("theme");
+  if (savedTheme === "dark") {
+    body.classList.add("dark-mode");
+    toggle.checked = true;
+  }
 });
